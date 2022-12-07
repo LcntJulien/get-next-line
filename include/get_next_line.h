fdcc31d5930547b3dd68d40d6394a3d16455e9dc
Julien <73828002+LcntJulien@users.noreply.github.com>
2022-12-07 15:01:33 +0100

    .

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -6,11 +6,13 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 18:07:05 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/12/06 18:14:50 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/12/07 14:57:45 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
+
+char    *get_next_line(int  fd);
 
 #endif
