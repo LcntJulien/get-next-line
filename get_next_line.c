166c2035252cd8cf7169beb83fc7e59ef64c4366
Julien <73828002+LcntJulien@users.noreply.github.com>
2022-12-07 16:31:54 +0100

    define BUFFER_SIZE / create variables on gnl.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,16 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 18:05:10 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/12/07 15:01:25 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/12/07 16:31:18 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char    *get_next_line(int  fd)
 {
-    
+    int r;
+    void    *buf[BUFFER_SIZE + 1];
+    static  char    *stash[1000];
+
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,18 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 18:07:05 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/12/07 14:57:45 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/12/07 16:28:24 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H BUFFER_SIZE
 #define GET_NEXT_LINE_H
+#define BUFFER_SIZE = 1
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
 
 char    *get_next_line(int  fd);
 
