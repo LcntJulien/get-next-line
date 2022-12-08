7dd5ae8f79caf1ded76f324706e2503249765248
Julio <73828002+LcntJulien@users.noreply.github.com>
2022-12-08 17:49:09 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 18:05:10 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/12/08 16:48:38 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/12/08 17:48:13 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,47 +22,51 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-int	process(char *line, int fd)
+//	boucle de read sur fd qui add buf a un stash(statique) a chaques iterations
+//		if qui stop le processus apres avoir call sur une fct pour stash si on trouve nextline
+// fct(aim 1) : sortir la line sans $ et ajouter un retour a la ligne (malloc)
+// fct(aim 2) : nettoyer le stash
+
+int	process(char *buffer, int filedes)
 {
-	int			i;
-	int			j;
-	static char	*stash;
-	void		*buf[BUFFER_SIZE + 1];
+	int	i;
 
 	i = 0;
-	j = BUFFER_SIZE;
-	stash = 0;
-	while (i != 0)
-	{
-		i = read(fd, buf, BUFFER_SIZE);
-		ft_strjoin(stash, buf);
-		while (stash[j++])
-		{
-			if (stash[j] == "$")
-			{
-				line = treatline(stash);
-				stash = treatstash(stash);
-				break ;
-			}
-		}
-	}
-	return (line);
-
-	//	boucle de read sur fd qui add buf a un stash(statique) a chaques iterations
-	//		if qui stop le processus apres avoir call sur une fct pour stash si on trouve nextline
-
-	// fct(aim 1) : sortir la line sans $ et ajouter un retour a la ligne (malloc)
-	// fct(aim 2) : nettoyer le stash
 }
 
 char	*get_next_line(int fd)
 {
-	char	*line;
+	char		*line;
+	static char	*buf;
 
 	line = 0;
-	if (!fd)
+	if (!fd || BUFFER_SIZE <= 0)
 		return (NULL);
 	else
-		line = process(line, fd);
+		process(buf, fd);
 	return (line);
 }
+
+// int			i;
+// int			j;
+// static char	*stash;
+// void		*buf[BUFFER_SIZE + 1];
+
+// i = 0;
+// j = BUFFER_SIZE;
+// stash = 0;
+// while (i != 0)
+// {
+// 	i = read(fd, buf, BUFFER_SIZE);
+// 	ft_strjoin(stash, buf);
+// 	while (stash[j++])
+// 	{
+// 		if (stash[j] == "$")
+// 		{
+// 			line = treatline(stash);
+// 			stash = treatstash(stash);
+// 			break ;
+// 		}
+// 	}
+// }
+// return (line);
