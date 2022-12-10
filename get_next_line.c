0d756e38ea4108ca6142f728ee7b5df4c575cae8
Julio <73828002+LcntJulien@users.noreply.github.com>
2022-12-10 13:35:51 +0100

    update gnl.c & gnlu.c
    making gnl shorter by modifying the main fn, same for gnlu by letting just the needed fns

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,57 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 18:05:10 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/12/08 17:48:13 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/12/10 13:17:27 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
+
+
+int	findnextline(char *stash)
 {
 	int	i;
+	int	j;
 
 	i = 0;
-	while (s[i])
-		i++;
-	return (i);
+	j = ft_strlen(stash);
+	while (stash[i++])
+	{
+		if (stash[i] == '\n')
+			return (1);
+	}
+	return (NULL);
 }
-
-//	boucle de read sur fd qui add buf a un stash(statique) a chaques iterations
-//		if qui stop le processus apres avoir call sur une fct pour stash si on trouve nextline
-// fct(aim 1) : sortir la line sans $ et ajouter un retour a la ligne (malloc)
-// fct(aim 2) : nettoyer le stash
 
 int	process(char *buffer, int filedes)
 {
-	int	i;
+	int		i;
+	char	*stash;
+	char	*line;
 
 	i = 0;
+	stash = 0;
+	line = 0;
+	while (i > 0)
+	{
+		i = read(filedes, buffer, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free(buffer);
+			break ;
+		}
+		buffer[i] = '\0';
+		ft_strjoin(stash, buffer);
+		if (findnextline(stash) > 0)
+		{
+			line = makeline(stash);
+			buffer = cleanbuf(buffer);
+			break ;
+		}
+	}
+	free(buffer);
+	return (line);
 }
 
 char	*get_next_line(int fd)
@@ -39,34 +64,16 @@ char	*get_next_line(int fd)
 	char		*line;
 	static char	*buf;
 
+	buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	line = 0;
 	if (!fd || BUFFER_SIZE <= 0)
 		return (NULL);
 	else
-		process(buf, fd);
+		line = process(buf, fd);
 	return (line);
 }
 
-// int			i;
-// int			j;
-// static char	*stash;
-// void		*buf[BUFFER_SIZE + 1];
-
-// i = 0;
-// j = BUFFER_SIZE;
-// stash = 0;
-// while (i != 0)
-// {
-// 	i = read(fd, buf, BUFFER_SIZE);
-// 	ft_strjoin(stash, buf);
-// 	while (stash[j++])
-// 	{
-// 		if (stash[j] == "$")
-// 		{
-// 			line = treatline(stash);
-// 			stash = treatstash(stash);
-// 			break ;
-// 		}
-// 	}
-// }
-// return (line);
+//	boucle de read sur fd qui add buf a un stash(statique) a chaques iterations
+//		if qui stop le processus apres avoir call sur une fct pour stash si on trouve nextline
+// fct(aim 1) : sortir la line sans $ et ajouter un retour a la ligne (malloc)
+// fct(aim 2) : nettoyer le stash
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,11 +6,21 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 18:06:39 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/12/08 14:16:24 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/12/10 13:12:57 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -37,61 +47,4 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	}
 	p[i + j] = '\0';
 	return (p);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	char	*p;
-	int		size;
-
-	i = 0;
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	if (len < ft_strlen(s + start))
-		size = len;
-	else
-		size = ft_strlen(s + start);
-	p = malloc(sizeof(char) * (size + 1));
-	if (!p)
-		return (NULL);
-	while (*(s + start) && i < len)
-	{
-		*(char *)(p + i) = *(char *)(s + start);
-		start++;
-		i++;
-	}
-	p[i] = '\0';
-	return (p);
-}
-
-static int	checkset(char c, const char *set)
-{
-	while (*set)
-	{
-		if (c == *set++)
-			return (1);
-	}
-	return (0);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	int		i;
-	int		j;
-	char	*p;
-
-	i = 0;
-	j = ft_strlen(s1);
-	if (s1 != 0)
-	{
-		while (*(s1 + i) && checkset(*(s1 + i), set))
-			i++;
-		if (i == j)
-			return (p = ft_strdup(""));
-		while (checkset(*(s1 + j - 1), set) && *(s1 + j - 1))
-			j--;
-		return (p = ft_substr(s1, i, (j - i)));
-	}
-	return (0);
 }
